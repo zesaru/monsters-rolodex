498c9d63201dc75aa7df90dc1a45e103aa456a24
Cesar Murillo <namiayumis@gmail.com>
2020-03-24 12:20:55 +0900

    fetching content

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          id: "01",
-          name: "Frankie"
-        },
-        {
-          id: "02",
-          name: "Dracula"
-        },
-        {
-          id: "03",
-          name: "zombie"
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json()) //convertirlo a json
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
     return (
