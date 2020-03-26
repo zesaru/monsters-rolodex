f4b6db9e90c6cc12dd6d98bd43daf4f7bb429302
Cesar Murillo <namiayumis@gmail.com>
2020-03-26 12:58:23 +0900

    Updates

---
+++ precache-manifest.0810cec72b02b111c682a5afdf5b0713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d47cdb4fa24115e1a129c13a44f1b1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "20bdbfc4830648360505",
+    "url": "/monsters-rolodex/static/css/main.3e558dc7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20bdbfc4830648360505",
+    "url": "/monsters-rolodex/static/js/main.cfaba0d0.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
